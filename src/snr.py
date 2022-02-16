f833b7531884b5bd727fa96025111d4f4a629e53
sxu0 <sxu0726@gmail.com>
2022-02-15 21:04:47 -0500

    plotted 1/noise, cleaned up output fig names

--- src/snr.py
+++ src/snr.py
@@ -8,53 +8,171 @@ Standalone script.
 """
 
 from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-no_scans = [1, 2, 4, 8, 16, 32, 64]
-no_scans_log2 = [0, 1, 2, 3, 4, 5, 6]
-peak_to_peak_noise = [0.07548, 0.06194, 0.06584, 0.06507, 0.06308, 0.06230, 0.06341]
-rms_noise = [0.01866, 0.01782, 0.01841, 0.01832, 0.01825, 0.01821, 0.01821]
+## data
+no_scans = np.array([1, 2, 4, 8, 16, 32, 64])
+no_scans_log2 = np.log2(no_scans)
+no_scans_sqrt = np.sqrt(no_scans)
+p2p_noise = np.array([0.07548, 0.06194, 0.06584, 0.06507, 0.06308, 0.06230, 0.06341])
+rms_noise = np.array([0.01866, 0.01782, 0.01841, 0.01832, 0.01825, 0.01821, 0.01821])
 
-df_noise = pd.DataFrame({"no_scans": no_scans, "peak_to_peak": peak_to_peak_noise, "rms": rms_noise})
+df_noise = pd.DataFrame({"no_scans": no_scans, "peak_to_peak": p2p_noise, "rms": rms_noise})
 df_noise.set_index("no_scans", inplace=True)
 # print(df_noise)
 
+## output directory
 path_save = Path.cwd() / "outputs" / "snr"
 try:
     Path.mkdir(path_save)
 except OSError:
     pass
 
-plt.figure()
-plt.plot(no_scans, peak_to_peak_noise, ".", label="peak to peak")
-plt.title("Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
-plt.xlabel("No. of Averaged Scans")
-plt.ylabel("Peak-to-Peak Noise (arbitrary single beam units)")
-fig_name = "p2p_noise_vs_scan_count.png"
-plt.savefig(path_save / fig_name)
+def plot_p2p(save_fig=False):
 
-plt.figure()
-plt.plot(no_scans, rms_noise, ".", label="RMS")
-plt.title("RMS Noise in 2603-2398 cm$^{-1}$ Region")
-plt.xlabel("No. of Averaged Scans")
-plt.ylabel("RMS Noise (arbitrary single beam units)")
-fig_name = "rms_noise_vs_scan_count.png"
-plt.savefig(path_save / fig_name)
+    plt.figure()
+    plt.plot(no_scans, p2p_noise, ".", label="peak to peak")
+    plt.title("Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("No. of Averaged Scans")
+    plt.ylabel("Peak-to-Peak Noise (arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "p2p_noise_vs_scan_count.png")
+        plt.close()
 
-plt.figure()
-plt.plot(no_scans_log2, peak_to_peak_noise, ".", label="peak to peak")
-plt.title("Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
-plt.xlabel("log$_2$(No. of Averaged Scans)")
-plt.ylabel("Peak-to-Peak Noise (arbitrary single beam units)")
-fig_name = "log_p2p_noise_vs_scan_count.png"
-plt.savefig(path_save / fig_name)
+    plt.figure()
+    plt.plot(no_scans_log2, p2p_noise, ".", label="peak to peak")
+    plt.title("Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("log$_2$(No. of Averaged Scans)")
+    plt.ylabel("Peak-to-Peak Noise (arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "p2p_noise_vs_log_scan_count.png")
+        plt.close()
 
-plt.figure()
-plt.plot(no_scans_log2, rms_noise, ".", label="RMS")
-plt.title("RMS Noise in 2603-2398 cm$^{-1}$ Region")
-plt.xlabel("log$_2$(No. of Averaged Scans)")
-plt.ylabel("RMS Noise (arbitrary single beam units)")
-fig_name = "log_rms_noise_vs_scan_count.png"
-plt.savefig(path_save / fig_name)
+    ## recall that SNR goes up as sqrt(N), N being scan count
+    plt.figure()
+    plt.plot(no_scans_sqrt, p2p_noise, ".", label="peak to peak")
+    plt.title("Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("sqrt(No. of Averaged Scans)")
+    plt.ylabel("Peak-to-Peak Noise (arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "p2p_noise_vs_sqrt_scan_count.png")
+        plt.close()
+
+    if not save_fig:
+        plt.show()
+
+    return
+
+def plot_rms(save_fig=False):
+
+    plt.figure()
+    plt.plot(no_scans, rms_noise, ".", label="RMS")
+    plt.title("RMS Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("No. of Averaged Scans")
+    plt.ylabel("RMS Noise (arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "rms_noise_vs_scan_count.png")
+        plt.close()
+
+    plt.figure()
+    plt.plot(no_scans_log2, rms_noise, ".", label="RMS")
+    plt.title("RMS Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("log$_2$(No. of Averaged Scans)")
+    plt.ylabel("RMS Noise (arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "rms_noise_vs_log_scan_count.png")
+        plt.close()
+
+    ## recall that SNR goes up as sqrt(N), N being scan count
+    plt.figure()
+    plt.plot(no_scans_sqrt, rms_noise, ".", label="RMS")
+    plt.title("RMS Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("sqrt(No. of Averaged Scans)")
+    plt.ylabel("RMS Noise (arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "rms_noise_vs_sqrt_scan_count.png")
+        plt.close()
+
+    if not save_fig:
+        plt.show()
+
+    return
+
+def plot_inv_p2p(save_fig=False):
+
+    plt.figure()
+    plt.plot(no_scans, 1/p2p_noise, ".", label="peak to peak")
+    plt.title("1/Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("No. of Averaged Scans")
+    plt.ylabel("1/Peak-to-Peak Noise (1/arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "inv_p2p_noise_vs_scan_count.png")
+        plt.close()
+
+    plt.figure()
+    plt.plot(no_scans_log2, 1/p2p_noise, ".", label="peak to peak")
+    plt.title("1/Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("log$_2$(No. of Averaged Scans)")
+    plt.ylabel("1/Peak-to-Peak Noise (1/arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "inv_p2p_noise_vs_log_scan_count.png")
+        plt.close()
+
+    ## recall that SNR goes up as sqrt(N), N being scan count
+    plt.figure()
+    plt.plot(no_scans_sqrt, 1/p2p_noise, ".", label="peak to peak")
+    plt.title("1/Peak-to-Peak Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("sqrt(No. of Averaged Scans)")
+    plt.ylabel("1/Peak-to-Peak Noise (1/arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "inv_p2p_noise_vs_sqrt_scan_count.png")
+        plt.close()
+
+    if not save_fig:
+        plt.show()
+
+    return
+
+def plot_inv_rms(save_fig=False):
+
+    plt.figure()
+    plt.plot(no_scans, 1/rms_noise, ".", label="RMS")
+    plt.title("1/RMS Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("No. of Averaged Scans")
+    plt.ylabel("1/RMS Noise (1/arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "inv_rms_noise_vs_scan_count.png")
+        plt.close()
+
+    plt.figure()
+    plt.plot(no_scans_log2, 1/rms_noise, ".", label="RMS")
+    plt.title("1/RMS Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("log$_2$(No. of Averaged Scans)")
+    plt.ylabel("1/RMS Noise (1/arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "inv_rms_noise_vs_log_scan_count.png")
+        plt.close()
+
+    ## recall that SNR goes up as sqrt(N), N being scan count
+    plt.figure()
+    plt.plot(no_scans_sqrt, 1/rms_noise, ".", label="RMS")
+    plt.title("1/RMS Noise in 2603-2398 cm$^{-1}$ Region")
+    plt.xlabel("sqrt(No. of Averaged Scans)")
+    plt.ylabel("1/RMS Noise (1/arbitrary single beam units)")
+    if save_fig:
+        plt.savefig(path_save / "inv_rms_noise_vs_sqrt_scan_count.png")
+        plt.close()
+
+    if not save_fig:
+        plt.show()
+
+    return
+
+if __name__ == "__main__":
+
+    plot_inv_p2p(save_fig=True)
+
+    plot_inv_rms(save_fig=True)
